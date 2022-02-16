0964643ac7a8e30c0fbd72bfa09c1e88de3ecf5f
Niyireth Cortes <niyirethcortes@periferia-it.com>
2022-02-16 11:50:09 -0500

    conexion php

--- Application/database.php
+++ Application/database.php
@@ -3,9 +3,11 @@
 	$username = "ncortes";
 	$password = "SigntoAll_";
 	$database = "Sign_to_all";
+	$connectionInfo = array( "Database"=>"Sign_to_all", "UID"=>"ncortes", "PWD"=>"SigntoAll_");
 
 	try {
-		$conn= new PDO("mysql:host=$server; dbname=$database;", $username, $password);
+		$conn = sqlsrv_connect( $server, $connectionInfo);
+
 
 	} catch (Exception $e) {
 		die("Conexion Fallida: " . $e->getMessage());
